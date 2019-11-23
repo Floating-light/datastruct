49a594273d875315d0d301c287be335b15a7b9f1
Floating-light <1308337629@qq.com>
2019-11-23 09:11:26 +0800

    feat complete swap

--- primer/chapter13/swap/swap.cpp
+++ primer/chapter13/swap/swap.cpp
@@ -2,6 +2,8 @@
 
 class HasPtr
 {
+friend void swap(HasPtr&, HasPtr&);
+
 public:
     HasPtr(const int& in, const std::string& sin = std::string()):
     a(in), pstr(new std::string(sin))
@@ -13,12 +15,18 @@ public:
     {
 
     }
-    HasPtr& operator=(const HasPtr& right)
+    HasPtr& operator=(HasPtr right)
     {
-        std::string* temp = new std::string(*right.pstr);
+        /* std::string* temp = new std::string(*right.pstr);
         delete pstr;
         pstr = temp;
-        a = right.a;
+        a = right.a; */
+        // using swap version copy-assignment, the parameter is not reference.
+        swap(*this, right);
+        return *this;
+        // self assignment and exception salf?
+        // only code might throw is the new expression indise the copy constructor,
+        // if occurs, it will happen before we have changed the left-hand operand.
     }
 
     ~HasPtr()
@@ -35,4 +43,31 @@ private:
 //v1 = v2;
 //v2 = temp; //copy assignment in origin v1
 //it also allocates memory.
-//that is unnecessary.
+//that is unnecessary.But we can define ourself version swap function for this class.
+inline void swap(HasPtr& l, HasPtr& r)
+{
+    using std::swap; //if memeber type has not itself swap function, will call std::swap.
+    swap(l.a, r.a);
+    swap(l.pstr, r.pstr);
+}
+
+class Foo
+{
+    friend void swap(Foo&, Foo&);
+    HasPtr h;
+};
+
+inline void swap(Foo& l, Foo& r)
+{
+    //this function use the library version of swap, not the HasPtr function version.
+    //this code would complie and execute.
+    //this is a error useage.
+    std::swap(l.h, r.h);
+}
+
+//correct version
+inline void swap(Foo& l, Foo& r)
+{
+    using std::swap;
+    swap(l.h, r.h);//there is a type-specific version of swap, will call this version preferentially.(16.3.p697)(18.2.3.p798)
+}
--- sundrycode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(){
-    int a = 7;
-    std::cout << "====> "<< (a = 12) << std::endl;
-
-    return 0;
-}
