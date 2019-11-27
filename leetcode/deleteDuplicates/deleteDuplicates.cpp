b1daed1409b237b41fb71c8f380658f724419a48
Floating-light <1308337629@qq.com>
2019-11-27 13:38:42 +0800

    feat briefness delete duplication

--- leetcode/deleteDuplicates/deleteDuplicates.cpp
+++ leetcode/deleteDuplicates/deleteDuplicates.cpp
@@ -34,3 +34,23 @@ ListNode* deleteDuplicates(ListNode* head)
     }
     return realHead->next;
 }
+
+ListNode* deleteDuplicatesV2(ListNode* head) 
+{
+    ListNode* realHead = new ListNode(0);
+    realHead->next = head;
+    
+    ListNode* p = realHead;
+    ListNode* p1 = p->next;
+    while(p1 != nullptr)
+    {
+        if(p1->next == nullptr || p->next->val != p1->next->val)
+        {
+            if(p->next == p1)
+                p = p1;
+            p->next = p1->next;   
+        }
+        p1 = p1->next;
+    }
+    return realHead->next;
+}
