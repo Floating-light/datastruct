eec158b38697b44f974bfe917ebbdbfd6f17c16c
Floating-light <1308337629@qq.com>
2019-12-04 18:06:49 +0800

    feat probing and backtracking

--- leetcode/combinationSum/combinationSum.cpp
+++ leetcode/combinationSum/combinationSum.cpp
@@ -53,11 +53,100 @@ vector<vector<int>> combinationSum(vector<int>& candidates, int target)
     }
     return result;
 }
+// 12ms
+vector<vector<int>> combinationSumReal(vector<int>& candidates, int target)
+{
+	if (candidates.size() < 1)
+		return vector<vector<int>>{};
+	const int length = candidates.size();
+	std::sort(candidates.begin(), candidates.end());
+	vector<vector<int>> result;
+
+	std::vector<int> index{};
+	int currentSum = 0;
+	int nextTryIndex = 0;
+
+	while (!index.empty() || nextTryIndex != length)
+	{
+		while (currentSum < target && nextTryIndex != length)
+		{
+			currentSum += candidates[nextTryIndex];
+			index.push_back(nextTryIndex);
+		}
+		if (currentSum == target)
+		{
+			vector<int> temp;
+			for (int i = 0; i < index.size(); ++i)
+			{
+				temp.push_back(candidates[index[i]]);
+			}
+			result.push_back(temp);
+		}
+
+		if (length != nextTryIndex)
+		{
+			currentSum -= candidates[index.back()];
+			index.pop_back(); // remove the last candidate
+		}
+		if (index.empty())
+		{
+			break;
+		}
+		nextTryIndex = 1 + index.back();
+
+		currentSum -= candidates[index.back()];
+		index.pop_back();
+	}
+
+	return result;
+}
+
+void DFS(vector<int> &candidates, int currentSum, 
+const int& next, const int& target, vector<vector<int>> &result, vector<int>& path)
+{
+    if(currentSum == target)
+    {
+        result.push_back(path);
+        return;
+    }
+    for ( int i = next; i < candidates.size(); ++i)
+    {
+        const int nextSum = candidates[i] + currentSum;
+        if(nextSum <= target)
+        {
+            path.push_back(candidates[i]);
+            DFS(candidates, nextSum, next, target, result, path);
+        }
+        else
+        {
+            break;
+        }
+    }
+        
+}
+
+vector<vector<int>> combinationSumRealDFS(vector<int>& candidates, int target)
+{
+    if(candidates.empty())
+    {
+        return vector<vector<int>>{};
+    }
+
+}
 
 int main() 
 {
     //vector<int> input = {1, 2,3,5,8,4,6,7,8,9,10};
     //vector<int> input = {1, 2,3,5,8,4,6,7,8,9,10};
-    vector<int> input = {};
-    combinationSum(input, 8);
+    vector<int> input = {2,3,5};
+    vector<vector<int>> result = combinationSumReal(input, 8);
+    for(int i = 0; i < result.size(); ++i)
+    {
+        std::cout << "result " << i+1 << ": ";
+        for(int j = 0; j < result[i].size(); ++j)
+        {
+            std::cout << result[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
 }
