d7459bde8a89c0856c0cc69a11deea5fd97128cc
Floating-light <1308337629@qq.com>
2019-12-05 18:43:43 +0800

    feat clear

--- primer/chapter13/5MgrDynamicMemory/main.cpp
+++ primer/chapter13/5MgrDynamicMemory/main.cpp
@@ -5,11 +5,6 @@
 int main() 
 {
     StrVec v;
-    v.push_back("test");
-    v.push_back("elemet2");
-    v.push_back("elemet3");
-    v.push_back("elemet4");
-    v.push_back("elemet5");
     v.push_back("elemet6");
     
     std::cout << "test" << std::endl;
