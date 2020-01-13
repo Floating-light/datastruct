936597d039f08fda7e8578fb8446faedf90a1628
Floating-light <liuyuanyu12138@gmail.com>
2020-01-13 21:59:05 +0800

    feat inheritance

--- primer/chapter15/inheritance.cpp
+++ primer/chapter15/inheritance.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 
-class Quote
+class Quote 
 {
 public:
     Quote() = default;
@@ -23,15 +23,27 @@ protected:
     double price = 0.0;
 };
 
-class Bulk_quote : public Quote 
+class Bulk_quote final: public Quote 
 {
 public:
     Bulk_quote() = default;
-    Bulk_quote(const std::string&, double, std::size_t, double);
-    /* virtual <optional>*/double net_price( std::size_t n) const override;
+    Bulk_quote(const std::string& book, double sales_price, std::size_t qty, double disc) : 
+        Quote(book, sales_price), min_qty(qty), discount(disc) { }
+    /* virtual <optional>*/double net_price( std::size_t n) const override
+    {
+        if( n > min_qty)
+            return n * (1 - discount) * price;
+        else
+            return n * price;
+    }
 private:
     std::size_t min_qty = 0;
     double discount = 0.0;
+};
+
+class t : Bulk_quote
+{
+
 };
 
 double print_total(std::ostream &os, const Quote& item, std::size_t m)
@@ -40,5 +52,12 @@ double print_total(std::ostream &os, const Quote& item, std::size_t m)
     os << "ISBN: " << item.isbn()
        << " # sold: " << m << " total due: " << ret << std::endl;
     return ret;
+}
+
+int main()
+{
+    Bulk_quote bq;
+    Quote* p = &bq;
+    Quote& r = bq; 
 }
  
