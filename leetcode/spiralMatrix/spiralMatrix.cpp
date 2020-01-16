6e4cfe5268581172fecc6575e2ef272c07d57620
Floating-light <liuyuanyu12138@gmail.com>
2020-01-16 21:22:58 +0800

    leetcode spiral matrix 100 prcentage

--- leetcode/spiralMatrix/spiralMatrix.cpp
+++ leetcode/spiralMatrix/spiralMatrix.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,14 +8,60 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> result;
         if( matrix.size() == 0 ) return result;
-        int h ,m = 0;
+        int h = 0 ,m = 0;
         int k = matrix[0].size();
         int n = matrix.size();
-        for( int i = h; i < k; ++i)
-            result.push_back(matrix[m][i]);
-        for( int i = m; m < n; ++i)
-            result.push_back(matrix[k-1][i]);
-        for();
-
+        while(h < k && m < n)
+        {
+            for( int i = h; i < k; ++i)
+                result.push_back(matrix[m][i]);
+            for( int i = m + 1; i < n; ++i)
+                result.push_back(matrix[i][k-1]);
+            if(n-1 != m)
+                for(int i = k - 2; i >= h ; --i)
+                    result.push_back(matrix[n - 1][i]);
+            if(k-1 != h)
+                for(int i = n - 2; i > m; --i)
+                    result.push_back(matrix[i][h]);
+            ++h;
+            ++m;
+            --k;
+            --n;
+        }
+        return result;
     }
 };
+
+int main()
+{
+    vector<vector<int>> input = {
+        {1,2,3,4,5,6}
+    };
+    vector<vector<int>> input2 = {
+        {1},
+        {2},
+        {3},
+        {4},
+        {5},
+    };
+    vector<vector<int>> input4 = {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+    vector<vector<int>> input3 = {
+        { 1, 2 },
+        { 4, 5 }
+    };
+    vector<vector<int>> input5 = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9,10,11,12}
+    };
+    vector<int> res = Solution().spiralOrder(input5);
+    for( int it : res)
+    {
+        std::cout << it << ", " << std::endl;
+    }
+    std::cout << "endl" << std::endl;
+}
