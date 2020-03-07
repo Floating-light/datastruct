6c93294fb7f25da8ee49a9c7aaed82e3c1fc8306
Floating-light <liuyuanyu12138@gmail.com>
2020-03-07 22:52:17 +0800

    CSChallengeCamp BC and BM algorithm

--- CSChallengeCamp/week4/Course_String.cpp
+++ CSChallengeCamp/week4/Course_String.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <functional>
@@ -91,18 +92,104 @@ public:
 		return i - j;
 	}
 
-	// BC O(n/m) ~ O(n*m)
+	vector<int> buildBC(const string& P)
+	{
+		vector<int> bc(256, -1);
+		for (int i = 0; i < P.size(); ++i)
+		{
+			bc[P[i]] = i;
+		}
+		return bc;
+	}
+
+	vector<int> buildSS(const string& P)
+	{
+		int m = P.size();
+		vector<int> ss(m, -1);
+		ss[m - 1] = m;
+		for (int lo = m - 1, hi = m - 1, j = lo - 1; j >= 0; j--)
+		{
+			if ((lo < j) && ss[m - hi + j - 1] <= j - lo)
+				ss[j] = ss[m - hi + j - 1];
+			else
+			{
+				hi = j;
+				lo = min(lo, hi);
+				while ((0 <= lo) && (P[lo] == P[m - hi + lo - 1])) lo--;
+				ss[j] = hi - lo;
+			}
+		}
+		return ss;
+	}
+
+	vector<int> buildGS(const string& P)
+	{
+		vector<int>ss = buildSS(P);
+		size_t m = P.size();
+		vector<int>gs(m, m);
+		for (size_t i = 0, j = m - 1; j < UINT_MAX; j--)
+		{
+			if (j + 1 == ss[j])
+				while (i < m - j - 1)
+					gs[i++] = m - j - 1;
+		}
+		for (size_t j = 0; j < m - 1; j++)
+		{
+			gs[m - ss[j] - 1] = m - j - 1;
+		}
+		return gs;
+	}
+
+	// BC O(n/m) ~ O(n*m) bad character
 	// 着重于失配带来的‘教训’
 	int matchBC(const string& P, const string& T)
 	{
+		vector<int> bc = buildBC(P);
 
+		int n = T.size(), i = 0;
+		int m = P.size();
+		while (n >= i + m)
+		{
+			int j = m - 1;
+			while (P[j] == T[i + j])
+				if (0 > --j) break;
+			if (0 > j) break; // 成功
+			else
+			{
+				// 不可能等
+				if (bc[T[i + j]] > j) i++;
+				else i += j - bc[T[i + j]];
+			}
+		}
+		return i;
 	}
+
+	// BM = BC + GS(Good suffix) O(m+n) ~ O(n/m)
+	int matchBM(const string& P, const string& T)
+	{
+		vector<int> bc = buildBC(P);
+		vector<int> gs = buildGS(P);
+
+		int m = T.size();
+		int n = P.size();
+		size_t i = 0;
+		while(m >= i + n)
+		{
+			int j = n - 1;
+			while (P[j] == T[i + j])
+				if (0 > --j) break;
+			if (0 > j) break;
+			else i += max(gs[j], j - bc[T[i + j]]);
+		}
+		return i;
+	}
+
 };
 
 int main()
 {
 	PatternMatching pm;
-	function<int(PatternMatching, const string&, const string&)> func = &PatternMatching::matchKMP;
+	function<int(PatternMatching, const string&, const string&)> func = &PatternMatching::matchBM;
 	int res = func(pm, "345", "1234567890");
 	cout << "P = 345, T = 1234567890 , restul: " << res << endl;
 	// int neg = -1;
